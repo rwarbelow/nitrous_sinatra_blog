a76da72178e03bd254c1f86fd8a2f016d5c3f6ab
rwarbelow <rachelwarbelow@gmail.com>
2014-03-23 05:54:51 +0000

    updated routes and views

--- app.rb
+++ app.rb
@@ -1,6 +1,55 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 require './environments'
+require 'sinatra/flash'
+require 'sinatra/redirect_with_flash'
+
+enable :sessions
+
+configure :development do
+  set :bind, '0.0.0.0'
+  set :port, 3000
+end
+
+helpers do
+  def title
+    if @title
+      "#{@title}"
+    else
+      "Welcome."
+    end
+  end
+end
 
 class Post < ActiveRecord::Base
+  validates :title, presence: true, length: { minimum: 5 }
+  validates :body, presence: true
+end
+
+
+get "/posts/create" do
+  @title = "Create Post"
+  @post = Post.new
+  erb :"posts/create"
+end
+
+get "/posts/:id" do 
+  @post = Post.find(params[:id])
+  @title = @post.title
+  erb :"posts/show"
+end
+
+post "/posts" do
+  @post = Post.new(params[:post])
+  if @post.save
+    redirect "posts/#{@post.id}", :notice => "Congrats! Love the new post"
+  else
+    erb :"posts/create", :error => "Something went wrong. Please try again."
+  end
+end
+
+get "/" do
+  @posts = Post.order("created_at DESC")
+  @title = "Welcome!"
+  erb :"posts/index"
 end
